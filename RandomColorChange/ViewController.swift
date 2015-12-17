55fef0e2c43845cf1b2a305d0c9ec15abad0dab0
joshR2 <josh2cool365@aol.com>
2015-12-17 08:47:00 -0700

    Initial Commit

---
+++ RandomColorChange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RandomColorChange
+//
+//  Created by Roberts, Joshua on 12/17/15.
+//  Copyright © 2015 Roberts, Joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
